f88cfa9e50c00c74731fa093d7112972c8c358b4
TrisA <1628791325@qq.com>
2022-04-27 20:08:33 +0800

    Gui/项目结构优化

--- demo.py
+++ demo.py
@@ -7,63 +7,118 @@
 # Email:1628791325@QQ.com
 # -U2hhcmUlMjBhbmQlMjBMb3Zl-base64
 
-
+from tkinter import *
 import cv2
 import numpy as np
+# import time
 
-image="./3.png"
+PCT = "./Photo_test/31.jpg"
+Image = cv2.imread(PCT)
+Size = Image.shape
 
-def ellipse_detect(image):
-    """
-    :param image: 图片路径
-    :return: None
-    """
-    # image: ".\test.png"
-    img = cv2.imread(image, cv2.IMREAD_COLOR)
-    skinCrCbHist = np.zeros((256, 256), dtype=np.uint8)
-    cv2.ellipse(skinCrCbHist, (113, 155), (23, 15), 43, 0, 360, (255, 255, 255), -1)
+# window = Tk()
+# window.title("test")
+# window.mainloop()
+# def readimg():
+#     image = cv2.imread(PCT)
+#     size = image.shape
+#     return image, size
 
-    YCRCB = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
-    (y, cr, cb) = cv2.split(YCRCB)
-    skin = np.zeros(cr.shape, dtype=np.uint8)
-    (x, y) = cr.shape
-    for i in range(0, x):
-        for j in range(0, y):
-            CR = YCRCB[i, j, 1]
-            CB = YCRCB[i, j, 2]
-            if skinCrCbHist[CR, CB] > 0:
-                skin[i, j] = 255
-    cv2.namedWindow(image, cv2.WINDOW_NORMAL)
-    cv2.imshow(image, img)
-    dst = cv2.bitwise_and(img, img, mask=skin)
-    cv2.namedWindow("cutout", cv2.WINDOW_NORMAL)
-    cv2.imshow("cutout", dst)
+def morphology(img):  # 形态学操作
+    k = np.ones((3, 3), np.uint8)
+    open = cv2.morphologyEx(img, cv2.MORPH_OPEN, k)  # 开运算
+    close = cv2.morphologyEx(open, cv2.MORPH_CLOSE, k)  # 闭运算
+    cv2.namedWindow("open", cv2.WINDOW_NORMAL)
+    cv2.imshow("open", open)
+    cv2.namedWindow("close", cv2.WINDOW_NORMAL)
+    cv2.imshow("close", close)
     cv2.waitKey()
+    return close
 
 
-def cr_otsu(image):
-    """YCrCb颜色空间的Cr分量+Otsu阈值分割
-    :param image: 图片路径
-    :return: None
-    """
-    img = cv2.imread(image, cv2.IMREAD_COLOR)
-    ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+def region(img):
+    # 连通域分析
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img, connectivity=8)
 
-    (y, cr, cb) = cv2.split(ycrcb)
-    cr1 = cv2.GaussianBlur(cr, (5, 5), 0)
-    _, skin = cv2.threshold(cr1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # 查看各个返回值
+    # 连通域数量
+    print('num_labels = ', num_labels)
+    # 连通域的信息：对应各个轮廓的左上角坐标x、y、width、height和面积
+    print('stats = ', stats)
+    # 连通域的中心点
+    print('centroids = ', centroids)
+    # 每一个像素的标签1、2、3.。。，同一个连通域的标签是一致的
+    print('labels = ', labels)
 
-    cv2.namedWindow("image raw", cv2.WINDOW_NORMAL)
-    cv2.imshow("image raw", img)
-    cv2.namedWindow("image CR", cv2.WINDOW_NORMAL)
-    cv2.imshow("image CR", cr1)
-    cv2.namedWindow("Skin Cr+OTSU", cv2.WINDOW_NORMAL)
-    cv2.imshow("Skin Cr+OTSU", skin)
+    # 不同的连通域赋予不同的颜色
+    output = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+    for i in range(1, num_labels):
+        mask = labels == i
+        output[:, :, 0][mask] = np.random.randint(0, 255)
+        output[:, :, 1][mask] = np.random.randint(0, 255)
+        output[:, :, 2][mask] = np.random.randint(0, 255)
+    # cv2.namedWindow("region", cv2.WINDOW_NORMAL)
+    # cv2.imshow('region', output)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
 
-    dst = cv2.bitwise_and(img, img, mask=skin)
-    cv2.namedWindow("seperate", cv2.WINDOW_NORMAL)
-    cv2.imshow("seperate", dst)
-    cv2.waitKey()
+    return num_labels, stats
+
+
+# def cr_otsu(image):
+#     """YCrCb颜色空间的Cr分量+Otsu阈值分割
+#     :param image: 图片路径
+#     :return: None
+#     """
+#     img = cv2.imread(image, cv2.IMREAD_COLOR)
+#     ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+#
+#     (y, cr, cb) = cv2.split(ycrcb)
+#     cr1 = cv2.GaussianBlur(cr, (5, 5), 0)
+#     _, skin = cv2.threshold(cr1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+#
+#     # cv2.namedWindow("image raw", cv2.WINDOW_NORMAL)
+#     # cv2.imshow("image raw", img)
+#     # cv2.namedWindow("image CR", cv2.WINDOW_NORMAL)
+#     # cv2.imshow("image CR", cr1)
+#     # cv2.namedWindow("Skin Cr+OTSU", cv2.WINDOW_NORMAL)
+#     # cv2.imshow("Skin Cr+OTSU", skin)
+#     # morphology(skin)
+#     # # dst = cv2.bitwise_and(img, img, mask=skin)
+#     # # cv2.namedWindow("seperate", cv2.WINDOW_NORMAL)
+#     # # cv2.imshow("seperate", dst)
+#     # cv2.waitKey()
+#     return skin
+
+
+def selectskin(stats):
+    num = len(stats)
+    size_total = Size[1] * Size[0]
+    tips = []
+    for i in range(1, num):
+        long = max(stats[i][2], stats[i][3])
+        short = min(stats[i][2], stats[i][3])
+        # ratio = long / short
+        ratio = stats[i][3] / stats[i][2]
+        size = stats[i][4]
+        if 1.1 <= ratio <= 2 and size/size_total >= 0.001:
+            tips.append(i)
+        else:
+            continue
+    return tips
+
+def signskin(tips, stats):
+    if tips:
+        signimg = Image
+        for k in range(0, len(tips)):
+            i = tips[k]
+            pt1 = (stats[i][0], stats[i][1])
+            pt2 = (stats[i][0] + stats[i][2], stats[i][1] + stats[i][3])
+            signimg = cv2.rectangle(Image, pt1, pt2, (0, 255, 0), 2, 4)
+        return signimg
+    else:
+        print("Dont find face")
+        return Image
 
 
 def crcb_range_sceening(image):
@@ -74,28 +129,155 @@ def crcb_range_sceening(image):
     img = cv2.imread(image, cv2.IMREAD_COLOR)
     ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
     (y, cr, cb) = cv2.split(ycrcb)
-    print(cr,y,cb)
+    # print(cr,y,cb)
     skin = np.zeros(cr.shape, dtype=np.uint8)
     (x, y) = cr.shape
-    print(cr.shape)
+    # print(cr.shape)
     for i in range(0, x):
         for j in range(0, y):
             if (cr[i][j] > 140) and (cr[i][j]) < 175 and (cr[i][j] > 100) and (cb[i][j]) < 120:
                 skin[i][j] = 255
             else:
                 skin[i][j] = 0
-    cv2.namedWindow(image, cv2.WINDOW_NORMAL)
-    cv2.imshow(image, img)
-    cv2.namedWindow(image + "skin2 cr+cb", cv2.WINDOW_NORMAL)
-    cv2.imshow(image + "skin2 cr+cb", skin)
+    print(skin)
+    return skin
+    # cv2.namedWindow(image, cv2.WINDOW_NORMAL)
+    # cv2.imshow(image, img)
+    # cv2.namedWindow(image + "skin2 cr+cb", cv2.WINDOW_NORMAL)
+    # cv2.imshow(image + "skin2 cr+cb", skin)
+    #
+    # dst = cv2.bitwise_and(img, img, mask=skin)
+    # cv2.namedWindow("cutout", cv2.WINDOW_NORMAL)
+    # cv2.imshow("cutout", dst)
+    #
+    # cv2.waitKey()
 
-    dst = cv2.bitwise_and(img, img, mask=skin)
-    cv2.namedWindow("cutout", cv2.WINDOW_NORMAL)
-    cv2.imshow("cutout", dst)
-
+def main():
+    skin = crcb_range_sceening(PCT)
+    morskin = morphology(skin)
+    num_labels, stats = region(morskin)
+    skininstats = selectskin(stats)
+    # 画框
+    signimg = signskin(skininstats, stats)
+    cv2.namedWindow("region", cv2.WINDOW_NORMAL)
+    cv2.imshow('region', signimg)
     cv2.waitKey()
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
-    #ellipse_detect(image)
-    #cr_otsu(image)
-    crcb_range_sceening(image)
+    # ellipse_detect(image)
+    # cr_otsu(image)
+    main()
+    # crcb_range_sceening(image)
+    # face_date(image) #这个是后面那个自己想的代码 还没成功过
+
+
+# def ellipse_detect(image):
+#     """
+#     :param image: 图片路径
+#     :return: None
+#     """
+#     # image: ".\test.png"
+#     img = cv2.imread(image, cv2.IMREAD_COLOR)
+#     skinCrCbHist = np.zeros((256, 256), dtype=np.uint8)
+#     cv2.ellipse(skinCrCbHist, (113, 155), (23, 15), 43, 0, 360, (255, 255, 255), -1)
+#
+#     YCRCB = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+#     (y, cr, cb) = cv2.split(YCRCB)
+#     skin = np.zeros(cr.shape, dtype=np.uint8)
+#     (x, y) = cr.shape
+#     for i in range(0, x):
+#         for j in range(0, y):
+#             CR = YCRCB[i, j, 1]
+#             CB = YCRCB[i, j, 2]
+#             if skinCrCbHist[CR, CB] > 0:
+#                 skin[i, j] = 255
+#     cv2.namedWindow(image, cv2.WINDOW_NORMAL)
+#     cv2.imshow(image, img)
+#     dst = cv2.bitwise_and(img, img, mask=skin)
+#     cv2.namedWindow("cutout", cv2.WINDOW_NORMAL)
+#     cv2.imshow("cutout", dst)
+#     cv2.waitKey()
+
+
+# def crcb_range_sceening(image):
+#     """
+#     :param image: 图片路径
+#     :return: None
+#     """
+#     img = cv2.imread(image, cv2.IMREAD_COLOR)
+#     ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+#     (y, cr, cb) = cv2.split(ycrcb)
+#     # print(cr,y,cb)
+#     skin = np.zeros(cr.shape, dtype=np.uint8)
+#     (x, y) = cr.shape
+#     # print(cr.shape)
+#     for i in range(0, x):
+#         for j in range(0, y):
+#             if (cr[i][j] > 140) and (cr[i][j]) < 175 and (cr[i][j] > 100) and (cb[i][j]) < 120:
+#                 skin[i][j] = 255
+#             else:
+#                 skin[i][j] = 0
+#     print(skin)
+#     return skin
+#     # cv2.namedWindow(image, cv2.WINDOW_NORMAL)
+#     # cv2.imshow(image, img)
+#     # cv2.namedWindow(image + "skin2 cr+cb", cv2.WINDOW_NORMAL)
+#     # cv2.imshow(image + "skin2 cr+cb", skin)
+#     #
+#     # dst = cv2.bitwise_and(img, img, mask=skin)
+#     # cv2.namedWindow("cutout", cv2.WINDOW_NORMAL)
+#     # cv2.imshow("cutout", dst)
+#     #
+#     # cv2.waitKey()
+
+
+'''
+def face_date(image):
+    img = cv2.imread(image, cv2.IMREAD_COLOR)
+    ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+    (y, cr, cb) = cv2.split(ycrcb)
+    skin = np.zeros(cr.shape, dtype=np.uint8)
+    a, b = cr.shape
+    print(a,b)
+
+    for i in range(0, a):
+        for j in range(0, b):
+            if (cr[i][j] > 140) and (cr[i][j]) < 175 and (cr[i][j] > 100) and (cb[i][j]) < 120:
+                skin[i][j] = 255
+            else:
+                skin[i][j] = 0
+    isTop = 0
+
+    print(skin)
+
+    for i in range(0, a-1):
+        for j in range(0, b-1):
+            if skin[i][j] == 255 & isTop == 0:
+                top = [i, j]
+                isTop = 1
+    for i in range(0, a - 1):
+        for j in range(0, b - 1):
+            if skin[i][j] == 255 & skin[i][j - 1] == 0:
+                left = [i, j]
+    for i in range(0, a - 1):
+        for j in range(0, b - 1):
+            if skin[i][j] == 255 & skin[i][j + 1] == 0:
+                right = [i, j]
+    for i in range(0, a - 1):
+        for j in range(0, b - 1):
+            if skin[i][j] == 255 & skin[i+1][j] == 0:
+                down = [i, j]
+
+    # time.sleep(10)
+    x = top[0]+3
+    y = left[1]
+    w = right[1]-left[1]
+    h = down[0]-top[0]
+    print(top,left,right,down)
+    cv2.rectangle(img, (x,y), (x + w, y+h), (255, 255, 255), 1)
+    cv2.imshow("image", img)
+    cv2.imshow("image", skin)
+    cv2.waitKey()
+'''
---
+++ testQT.py
@@ -0,0 +1,236 @@
+import sys
+import cv2 as cv
+import numpy as np
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QFileDialog, QMainWindow
+
+from UI.test import Ui_MainWindow
+
+
+
+class PyQtMainEntry(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.camera = cv.VideoCapture(0)
+        self.is_camera_opened = False  # 摄像头有没有打开标记
+
+        # 定时器：30ms捕获一帧
+        self._timer = QtCore.QTimer(self)
+        self._timer.timeout.connect(self._queryFrame)
+        self._timer.setInterval(30)
+
+    def btnOpenCamera_Clicked(self):
+        '''
+        打开和关闭摄像头
+        '''
+        self.is_camera_opened = ~self.is_camera_opened
+        if self.is_camera_opened:
+            self.btnOpenCamera.setText("关闭摄像头")
+            self._timer.start()
+        else:
+            self.btnOpenCamera.setText("打开摄像头")
+            self.labelCamera.clear()
+            self.labelCamera.setText("摄像头")
+            self._timer.stop()
+
+    def btnCapture_Clicked(self):
+        '''
+        捕获图片
+        '''
+        # 摄像头未打开，不执行任何操作
+        if not self.is_camera_opened:
+            return
+
+        self.captured = self.frame
+
+        # 后面这几行代码几乎都一样，可以尝试封装成一个函数
+        rows, cols, channels = self.captured.shape
+        bytesPerLine = channels * cols
+        # Qt显示图片时，需要先转换成QImgage类型
+        QImg = QImage(self.captured.data, cols, rows, bytesPerLine, QImage.Format_RGB888)
+        self.labelCapture.setPixmap(QPixmap.fromImage(QImg).scaled(
+            self.labelCapture.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+    def btnReadImage_Clicked(self):
+        '''
+        从本地读取图片
+        '''
+        # 打开文件选取对话框
+        filename, _ = QFileDialog.getOpenFileName(self, '打开图片')
+        if filename:
+            self.captured = cv.imread(str(filename))
+            # OpenCV图像以BGR通道存储，显示时需要从BGR转到RGB
+            self.captured = cv.cvtColor(self.captured, cv.COLOR_BGR2RGB)
+
+            rows, cols, channels = self.captured.shape
+            bytesPerLine = channels * cols
+            QImg = QImage(self.captured.data, cols, rows, bytesPerLine, QImage.Format_RGB888)
+            self.labelCapture.setPixmap(QPixmap.fromImage(QImg).scaled(
+                self.labelCapture.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+    def btnGray_Clicked(self):
+        '''
+        灰度化
+        '''
+        # 如果没有捕获图片，则不执行操作
+        if not hasattr(self, "captured"):
+            return
+
+        # self.cpatured = cv.cvtColor(self.captured, cv.COLOR_RGB2GRAY)
+        # 全局均衡化
+        # self.equalize = cv.equalizeHist(self.cpatured)
+        # 局部均衡化
+        ## createCLAHE(clipLimit=None, tileGridSize=None)
+        # clahe = cv.createCLAHE(tileGridSize=(5, 5))
+        # self.equalize = clahe.apply(self.cpatured)
+
+        # ycrcb = cv.cvtColor(self.captured, cv.COLOR_BGR2RGB)
+        r , g, b = cv.split(self.captured)
+        #R,G,B = cv.split(ycrcb)
+
+
+
+        # r =cv.equalizeHist(r)
+        # g = cv.equalizeHist(g)
+        # b =cv.equalizeHist(b)
+
+        clahe = cv.createCLAHE(clipLimit=0.5, tileGridSize=(8,8))
+        r = clahe.apply(r)
+        g = clahe.apply(g)
+        b = clahe.apply(b)
+
+        self.equalize = cv.merge((r,g,b))
+        self.captured = self.equalize
+        # self.equalize = cv.cvtColor(aaa)
+
+
+        # rows, columns = self.cpatured.shape
+        rows, columns, channels = self.equalize.shape
+        bytesPerLine = channels * columns
+        # 灰度图是单通道，所以需要用Format_Indexed8
+        QImg = QImage(self.equalize.data, columns, rows, bytesPerLine, QImage.Format_RGB888)
+        self.labelResult.setPixmap(QPixmap.fromImage(QImg).scaled(
+            self.labelResult.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+    def btnThreshold_Clicked(self):
+        '''
+        执行程序
+        '''
+        if not hasattr(self, "captured"):
+            return
+
+        # _, self.cpatured = cv.threshold(
+        #     self.cpatured, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
+        self.crcb_range_sceening()
+        self.morphology()
+        self.region()
+        self.selectskin()
+        self.signskin()
+
+        rows, cols, channels = self.result.shape
+        bytesPerLine = channels * cols
+        QImg = QImage(self.captured.data, cols, rows, bytesPerLine, QImage.Format_RGB888)
+        self.labelResult.setPixmap(QPixmap.fromImage(QImg).scaled(
+            self.labelResult.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+    def crcb_range_sceening(self):
+        """
+        # :param image: 图片路径
+        # :return: None
+        """
+        # self.img = cv.imread(, cv.IMREAD_COLOR)
+        ycrcb = cv.cvtColor(self.captured, cv.COLOR_RGB2YCR_CB)
+        (y, cr, cb) = cv.split(ycrcb)
+        # print(cr,y,cb)
+        self.skin = np.zeros(cr.shape, dtype=np.uint8)
+        (x, y) = cr.shape
+        # print(cr.shape)
+        for i in range(0, x):
+            for j in range(0, y):
+                if (cr[i][j] > 140) and (cr[i][j]) < 175 and (cr[i][j] > 100) and (cb[i][j]) < 120:
+                    self.skin[i][j] = 255
+                else:
+                    self.skin[i][j] = 0
+
+    def morphology(self):  # 形态学操作
+        k = np.ones((3, 3), np.uint8)
+        self.open = cv.morphologyEx(self.skin, cv.MORPH_OPEN, k)  # 开运算
+        self.close = cv.morphologyEx(self.open, cv.MORPH_CLOSE, k)  # 闭运算
+
+    def region(self):
+        # 连通域分析
+        num_labels, labels, self.stats, centroids = cv.connectedComponentsWithStats(self.close, connectivity=8)
+
+        # # 查看各个返回值
+        # # 连通域数量
+        # print('num_labels = ', num_labels)
+        # # 连通域的信息：对应各个轮廓的左上角坐标x、y、width、height和面积
+        # print('stats = ', stats)
+        # # 连通域的中心点
+        # print('centroids = ', centroids)
+        # # 每一个像素的标签1、2、3.。。，同一个连通域的标签是一致的
+        # print('labels = ', labels)
+
+        # 不同的连通域赋予不同的颜色
+        # self.output = np.zeros((self.close.shape[0], self.close.shape[1], 3), np.uint8)
+        # for i in range(1, num_labels):
+        #     mask = labels == i
+        #     self.output[:, :, 0][mask] = np.random.randint(0, 255)
+        #     self.output[:, :, 1][mask] = np.random.randint(0, 255)
+        #     self.output[:, :, 2][mask] = np.random.randint(0, 255)
+        # cv2.namedWindow("region", cv2.WINDOW_NORMAL)
+        # cv2.imshow('region', output)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+
+    def selectskin(self):
+        num = len(self.stats)
+        size_total = self.captured.shape[1] * self.captured.shape[0]
+        self.tips = []
+        for i in range(0, num):
+            long = max(self.stats[i][2], self.stats[i][3])
+            short = min(self.stats[i][2], self.stats[i][3])
+            # ratio = long / short
+            ratio = self.stats[i][3] / self.stats[i][2]
+            size = self.stats[i][4]
+            if 1.1 <= ratio <= 2 and size / size_total >= 0.001:
+                self.tips.append(i)
+            else:
+                continue
+
+    def signskin(self):
+        if self.tips:
+            self.result = self.captured
+            for k in range(0, len(self.tips)):
+                i = self.tips[k]
+                pt1 = (self.stats[i][0], self.stats[i][1])
+                pt2 = (self.stats[i][0] + self.stats[i][2], self.stats[i][1] + self.stats[i][3])
+                self.result = cv.rectangle(self.result, pt1, pt2, (0, 255, 0), 2, 4)
+        else:
+            self.result = self.captured
+            box = QtWidgets.QMessageBox()
+            box.warning(self, "提示", "Dont find face")
+            print("Dont find face")
+
+    @QtCore.pyqtSlot()
+    def _queryFrame(self):
+        '''
+        循环捕获图片
+        '''
+        ret, self.frame = self.camera.read()
+
+        img_rows, img_cols, channels = self.frame.shape
+        bytesPerLine = channels * img_cols
+
+        cv.cvtColor(self.frame, cv.COLOR_BGR2RGB, self.frame)
+        QImg = QImage(self.frame.data, img_cols, img_rows, bytesPerLine, QImage.Format_RGB888)
+        self.labelCamera.setPixmap(QPixmap.fromImage(QImg).scaled(
+            self.labelCamera.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+
